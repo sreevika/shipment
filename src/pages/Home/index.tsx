0488ff140b3addf8573cf5eff55cfacee17a1ae4
Prinju Koshy Vaidyan <prinjuvaidyan@gmail.com>
2023-06-21 21:03:45 +0530

    added hero banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -372,36 +372,37 @@ export default function HomePage() {
         </div>
       </nav> */}
       <nav className="header-navigation">
-        <div className="header-navigation__product-image">
-          <a href="#" className="header-navigation__logo-item header-navigation__ps-logo">
-            <img className="ps-logo__image" src={petsmartImg} alt="Pet Smart Logo" />
-          </a>
-          <a href="#" className="header-navigation__logo-item  header-navigation-pg-logo">
-            <img className="pg-logo__image" src={pangeaImg} alt="Pangea Logo" />
-          </a>
-        </div>
-        <ul className="header-navigation__menu">
-          <li className="header-navigation__menu-item">
-            <a href="#" className="header-navigation__menu-link active">
-              FedEx
+        <div className="container max-container header-navigation-container">
+          <div className="header-navigation__product-image">
+            <a href="#" className="header-navigation__logo-item header-navigation__ps-logo">
+              <img className="ps-logo__image" src={petsmartImg} alt="Pet Smart Logo" />
             </a>
-          </li>
-          <li className="header-navigation__menu-item">
-            <a href="#" className="header-navigation__menu-link">
-              UPS
+            <a href="#" className="header-navigation__logo-item  header-navigation-pg-logo">
+              <img className="pg-logo__image" src={pangeaImg} alt="Pangea Logo" />
             </a>
-          </li>
-        </ul>
-        <div className="header-navigation__search-bar">
-          <div className="header-navigation__search-select">
-            <button className="header-navigation__search-select-input">
-              Store ID
-              <svg width="8" height="4" viewBox="0 0 8 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0 0L3.75 3.75L7.5 0H0Z" fill="#737274" />
-              </svg>
-            </button>
-            {/* use normal click function to handle dropdown since this is custom */}
-            {/* <select
+          </div>
+          <ul className="header-navigation__menu">
+            <li className="header-navigation__menu-item">
+              <a href="#" className="header-navigation__menu-link active">
+                FedEx
+              </a>
+            </li>
+            <li className="header-navigation__menu-item">
+              <a href="#" className="header-navigation__menu-link">
+                UPS
+              </a>
+            </li>
+          </ul>
+          <div className="header-navigation__search-bar">
+            <div className="header-navigation__search-select">
+              <button className="header-navigation__search-select-input">
+                Store ID
+                <svg width="8" height="4" viewBox="0 0 8 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M0 0L3.75 3.75L7.5 0H0Z" fill="#737274" />
+                </svg>
+              </button>
+              {/* use normal click function to handle dropdown since this is custom */}
+              {/* <select
               className="header-navigation__search-select-input"
               value={searchType}
               onChange={searchTypeSelectionChange}
@@ -409,96 +410,104 @@ export default function HomePage() {
               <option value="storeId">Store ID</option>
               <option value="trackNo">Tracking Number</option>
             </select> */}
-            {/* add active class below to show the dropdown */}
-            <div className="search-select-dropdown">
-              <ul className="search-select-dp-wrapper">
-                <li className="search-select-dp-item">
-                  <a href="#">
-                    Store ID
-                  </a>
-                </li>
-                <li className="search-select-dp-item">
-                  <a href="#">
-                    Tracking number
-                  </a>
+              {/* add active class below to show the dropdown */}
+              <div className="search-select-dropdown">
+                <ul className="search-select-dp-wrapper">
+                  <li className="search-select-dp-item">
+                    <a href="#">
+                      Store ID
+                    </a>
+                  </li>
+                  <li className="search-select-dp-item">
+                    <a href="#">
+                      Tracking number
+                    </a>
 
-                </li>
-              </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-          <div className="header-navigation__search-input-box">
-            <div className="header-navigation__search-wrapper">
-              <div className="search__input-box-prefix">
-                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M12.5 11H11.71L11.43 10.73C12.41 9.59 13 8.11 13 6.5C13 2.91 10.09 0 6.5 0C2.91 0 0 2.91 0 6.5C0 10.09 2.91 13 6.5 13C8.11 13 9.59 12.41 10.73 11.43L11 11.71V12.5L16 17.49L17.49 16L12.5 11ZM6.5 11C4.01 11 2 8.99 2 6.5C2 4.01 4.01 2 6.5 2C8.99 2 11 4.01 11 6.5C11 8.99 8.99 11 6.5 11Z" fill="black" fill-opacity="0.54" />
-                </svg>
+            <div className="header-navigation__search-input-box">
+              <div className="header-navigation__search-wrapper">
+                <div className="search__input-box-prefix">
+                  <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12.5 11H11.71L11.43 10.73C12.41 9.59 13 8.11 13 6.5C13 2.91 10.09 0 6.5 0C2.91 0 0 2.91 0 6.5C0 10.09 2.91 13 6.5 13C8.11 13 9.59 12.41 10.73 11.43L11 11.71V12.5L16 17.49L17.49 16L12.5 11ZM6.5 11C4.01 11 2 8.99 2 6.5C2 4.01 4.01 2 6.5 2C8.99 2 11 4.01 11 6.5C11 8.99 8.99 11 6.5 11Z" fill="black" fill-opacity="0.54" />
+                  </svg>
+                </div>
+
+                <input
+                  type="text"
+                  className="search__input-box"
+                  placeholder="search..."
+                  value={searchValue}
+                  onChange={searchValueChange}
+                />
+                <button className="search__input-box-suffix">
+
+                  <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z" fill="#0067B2" />
+                  </svg>
+
+                </button>
               </div>
 
-              <input
-                type="text"
-                className="search__input-box"
-                placeholder="search..."
-                value={searchValue}
-                onChange={searchValueChange}
-              />
-              <button className="search__input-box-suffix">
+              {/* add active class below to show the dropdown */}
+              <div className="search-select-dropdown">
+                <ul className="search-select-dp-wrapper">
+                  <li className="search-select-dp-item">
+                    <a href="#">
+                      Store ID
+                    </a>
+                  </li>
+                  <li className="search-select-dp-item">
+                    <a href="#">
+                      Tracking number
+                    </a>
 
-                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z" fill="#0067B2" />
-                </svg>
-
-              </button>
-            </div>
-
-            {/* add active class below to show the dropdown */}
-            <div className="search-select-dropdown">
-              <ul className="search-select-dp-wrapper">
-                <li className="search-select-dp-item">
-                  <a href="#">
-                    Store ID
-                  </a>
-                </li>
-                <li className="search-select-dp-item">
-                  <a href="#">
-                    Tracking number
-                  </a>
-
-                </li>
-              </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="header-navigation__profile">
-          <ul className="header-navigation__actions">
-            <li className="header-navigation__item-actions">
-              <a href="#" className="header-navigation__item-actions-anchor">
-                <div className="header-navigation__item-actions-icon">
-                  <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1.74999 11.583C1.37533 11.583 1.05933 11.4547 0.801992 11.198C0.545326 10.9407 0.416992 10.6247 0.416992 10.25V1.74999C0.416992 1.37533 0.545326 1.05933 0.801992 0.801992C1.05933 0.545326 1.37533 0.416992 1.74999 0.416992H14.25C14.6247 0.416992 14.9407 0.545326 15.198 0.801992C15.4547 1.05933 15.583 1.37533 15.583 1.74999V10.25C15.583 10.6247 15.4547 10.9407 15.198 11.198C14.9407 11.4547 14.6247 11.583 14.25 11.583H1.74999ZM7.99999 6.62499L1.49999 2.81199V10.25C1.49999 10.3193 1.52433 10.3783 1.57299 10.427C1.62166 10.4757 1.68066 10.5 1.74999 10.5H14.25C14.3193 10.5 14.3783 10.4757 14.427 10.427C14.4757 10.3783 14.5 10.3193 14.5 10.25V2.81199L7.99999 6.62499ZM7.99999 5.22899L14.375 1.49999H1.62499L7.99999 5.22899ZM1.49999 2.81199V1.49999V10.25C1.49999 10.3193 1.52433 10.3783 1.57299 10.427C1.62166 10.4757 1.68066 10.5 1.74999 10.5H1.49999V10.25V2.81199Z" fill="#0067B2" />
-                  </svg>
-                </div>
-                <div className="header-navigation__item-actions-text">
-                  Alerts
-                </div>
-              </a>
-            </li>
-            <li className="header-navigation__item-actions">
-              <a href="#" className="header-navigation__item-actions-anchor">
-                <div className="header-navigation__item-actions-icon">
-                  <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1.833 14C1.45833 14 1.14233 13.8717 0.885 13.615C0.628333 13.3577 0.5 13.0417 0.5 12.667V1.74999C0.5 1.37533 0.628333 1.05933 0.885 0.801992C1.14233 0.545326 1.45833 0.416992 1.833 0.416992H7.104V1.49999H1.833C1.77767 1.49999 1.72233 1.52766 1.667 1.58299C1.611 1.63899 1.583 1.69466 1.583 1.74999V12.667C1.583 12.7223 1.611 12.7777 1.667 12.833C1.72233 12.889 1.77767 12.917 1.833 12.917H7.104V14H1.833ZM10.604 10.104L9.833 9.33299L11.417 7.74999H5.104V6.66699H11.417L9.833 5.08299L10.604 4.31199L13.5 7.20799L10.604 10.104Z" fill="#0067B2" />
-                  </svg>
-                </div>
-                <div className="header-navigation__item-actions-text">
-                  Logout
-                </div>
-              </a>
-            </li>
-          </ul>
+          <div className="header-navigation__profile">
+            <ul className="header-navigation__actions">
+              <li className="header-navigation__item-actions">
+                <a href="#" className="header-navigation__item-actions-anchor">
+                  <div className="header-navigation__item-actions-icon">
+                    <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M1.74999 11.583C1.37533 11.583 1.05933 11.4547 0.801992 11.198C0.545326 10.9407 0.416992 10.6247 0.416992 10.25V1.74999C0.416992 1.37533 0.545326 1.05933 0.801992 0.801992C1.05933 0.545326 1.37533 0.416992 1.74999 0.416992H14.25C14.6247 0.416992 14.9407 0.545326 15.198 0.801992C15.4547 1.05933 15.583 1.37533 15.583 1.74999V10.25C15.583 10.6247 15.4547 10.9407 15.198 11.198C14.9407 11.4547 14.6247 11.583 14.25 11.583H1.74999ZM7.99999 6.62499L1.49999 2.81199V10.25C1.49999 10.3193 1.52433 10.3783 1.57299 10.427C1.62166 10.4757 1.68066 10.5 1.74999 10.5H14.25C14.3193 10.5 14.3783 10.4757 14.427 10.427C14.4757 10.3783 14.5 10.3193 14.5 10.25V2.81199L7.99999 6.62499ZM7.99999 5.22899L14.375 1.49999H1.62499L7.99999 5.22899ZM1.49999 2.81199V1.49999V10.25C1.49999 10.3193 1.52433 10.3783 1.57299 10.427C1.62166 10.4757 1.68066 10.5 1.74999 10.5H1.49999V10.25V2.81199Z" fill="#0067B2" />
+                    </svg>
+                  </div>
+                  <div className="header-navigation__item-actions-text">
+                    Alerts
+                  </div>
+                </a>
+              </li>
+              <li className="header-navigation__item-actions">
+                <a href="#" className="header-navigation__item-actions-anchor">
+                  <div className="header-navigation__item-actions-icon">
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M1.833 14C1.45833 14 1.14233 13.8717 0.885 13.615C0.628333 13.3577 0.5 13.0417 0.5 12.667V1.74999C0.5 1.37533 0.628333 1.05933 0.885 0.801992C1.14233 0.545326 1.45833 0.416992 1.833 0.416992H7.104V1.49999H1.833C1.77767 1.49999 1.72233 1.52766 1.667 1.58299C1.611 1.63899 1.583 1.69466 1.583 1.74999V12.667C1.583 12.7223 1.611 12.7777 1.667 12.833C1.72233 12.889 1.77767 12.917 1.833 12.917H7.104V14H1.833ZM10.604 10.104L9.833 9.33299L11.417 7.74999H5.104V6.66699H11.417L9.833 5.08299L10.604 4.31199L13.5 7.20799L10.604 10.104Z" fill="#0067B2" />
+                    </svg>
+                  </div>
+                  <div className="header-navigation__item-actions-text">
+                    Logout
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
-
-      <div className="imgBanner">
+      <div className="hero-banner">
+        <div className="container mid-container hero-banner__container">
+          <div className="hero-banner__container--title">Shipment Dashboard</div>
+          <div className="hero-banner__container--date">
+            {lastDayDate} - {todayDate}
+          </div>
+        </div>
+      </div>
+      {/* <div className="imgBanner">
         <div className="container imgBanner-container">
           <div className="imgBanner-content">
             <div className="LabelHead">Shipment Dashboard</div>
@@ -508,8 +517,8 @@ export default function HomePage() {
           </div>
         </div>
         <div className="dashboard-pattern"></div>
-      </div>
-      <div className="GridPannel">
+      </div> */}
+      < div className="GridPannel" >
         <Grid container spacing={2} columns={16}>
           <Grid item xs={4}>
             <div
@@ -671,7 +680,7 @@ export default function HomePage() {
             </div>
           </Grid>
         </Grid>
-      </div>
+      </div >
       <div className="table-div">
         <Paper>
           <TableContainer sx={{ maxHeight: 500 }}>
