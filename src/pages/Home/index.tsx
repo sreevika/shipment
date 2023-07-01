706209eeeabad82e7ed1a575d4a4d2a627f06dd8
sreevika <sreevika0093@gmail.com>
2023-07-01 17:09:35 +0530

    build error-free

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -29,6 +29,9 @@ import {
 import axios from "axios";
 import _ from 'lodash';
 
+import shipment from "../../components/Interfaces";
+
+
 //generic method to format time
 function formatDate(dateTime: any, format: Intl.DateTimeFormatOptions): string {
   if (dateTime == null) return "";
@@ -37,62 +40,6 @@ function formatDate(dateTime: any, format: Intl.DateTimeFormatOptions): string {
   return formattedDateTime;
 }
 
-interface shipment {
-  id: number;
-  trackingNumberUniqueId: string;
-  trackingNumber: string;
-  deliveryCompany: string | null;
-  storeId: string | null;
-  storeDivision: string;
-  storeZip: string | null;
-  shipDate: string;
-  accountNumber: string;
-  purchaseOrderNumber: string | null;
-  masterTrackingNumber: string | null;
-  status: string;
-  statusCode: string;
-  statusDescription: string;
-  shipperName: string;
-  shipperCompany: string;
-  shipperCity: string;
-  shipperState: string;
-  scheduledDeliveryDate: string | null;
-  scheduledDeliveryTimeBefore: string | null;
-  recipientContactName: string | null;
-  recipientCompany: string | null;
-  recipientAddress: string | null;
-  recipientCity: string;
-  recipientState: string;
-  recipientCountry: string;
-  reference: string | null;
-  direction: string | null;
-  fedExCompany: string;
-  numberOfAttemptedDeliveries: number;
-  deliveredTime: string | null;
-  packageWeightKg: string;
-  packageWeightLbs: string;
-  receivedBy: string | null;
-  recipientPostal: string;
-  shippedBy: string;
-  shippedTo: string | null;
-  shipperAddress: string;
-  shipperCountry: string;
-  shipperPostal: string;
-  specialHandling: string;
-  serviceType: string;
-  totalWeightInKg: number;
-  totalWeightInLbs: number;
-  packageCount: number;
-  insertedTime: string;
-  updatedTime: string;
-  isDelayed: boolean;
-  isException: boolean;
-  isOnTime: boolean;
-  isDelivered: boolean;
-  isEarly: boolean;
-  isCancelled: boolean;
-  isOutForDelivery: boolean;
-}
 
 function dashboardIconPack(iconName: string) {
   switch (iconName) {
